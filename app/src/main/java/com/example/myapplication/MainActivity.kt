fd8ef1e73ba7186554d91423592bf5e8c5f5c60f
xaman <xaman282@gmail.com>
2025-06-02 00:04:19 +0530

    by aman from d9

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,7 +16,8 @@ class MainActivity : AppCompatActivity() {
 
         title = findViewById<TextView>(R.id.txt_title)
 
-        title.setText("Hllo welcome to kotlin/////////////////")
+        title.setText("Hllo welcome555555555555555555555")
+
 
     }
 }
