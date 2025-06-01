6da5ed21df784e7b5562379334933728081523ba
prajkta <waingankar.prajkta27@gmail.com>
2025-06-01 23:25:11 +0530

    "new branch category master"

---
+++ app/src/main/java/com/example/myapplication/CategoryActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_category)
+        println("Select the category==================")
+
+
+
+    }
+}
