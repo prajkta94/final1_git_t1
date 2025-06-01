38aec71ec28ba8a28dfea010f3fe013edea7de7a
prajkta <waingankar.prajkta27@gmail.com>
2025-06-01 17:59:10 +0530

    "new branch signIn master"

---
+++ app/src/main/java/com/example/myapplication/SignInActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_in)
+
+
+        println("Welcome to sign in=================")
+    }
+}
